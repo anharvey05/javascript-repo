841f9542a11db9a8fb0d170a660d0dce28210e82
anharvey05 <amy.harvey@onshoreoutsourcing.com>
2019-09-12 11:28:14 -0500

    Added pokemonList object & funct to loop through and display it

--- scripts/home.js
+++ scripts/home.js
@@ -1,9 +1,43 @@
 function buttonClick() {
-    if (document.getElementById('dateTimeText').innerHTML === "") {
+    if (document.getElementById('dateTimeText').innerHTML == "") {
         document.getElementById('dateTimeText').innerHTML = Date();
         document.getElementById('dateTimeButton').innerHTML = "Click to Collapse!";
     } else {
         document.getElementById('dateTimeText').innerHTML = "";
         document.getElementById('dateTimeButton').innerHTML = "Click for Date/Time!";
     }
+}
+
+var index = 0;
+var pokemonList = { 
+    "pokemon": [ 
+        { "name": "Bulbasaur",
+            "type": "Grass",
+            "height": "2'04",
+            "weight": "15.2lbs" },
+        { "name": "Charmander",
+            "type": "Fire",
+            "height": "2'0",
+            "weight": "18.7lbs" },
+        { "name": "Squirtle",
+            "type": "Water",
+            "height": "1'08",
+            "weight": "19.8lbs" }
+    ]
+};
+
+function displayPokemon() {
+    //When button is clicked, display current creature
+    //Update index value so next creature is displayed when clicked again
+    document.getElementById("nameText").innerHTML = pokemonList.pokemon[index].name;
+    document.getElementById("typeText").innerHTML = pokemonList.pokemon[index].type;
+    document.getElementById("heightText").innerHTML = pokemonList.pokemon[index].height;
+    document.getElementById("weightText").innerHTML = pokemonList.pokemon[index].weight;
+
+    if (index < (pokemonList.pokemon.length - 1)){
+        index++;
+    } else {
+        index = 0;
+    }
+
 }
